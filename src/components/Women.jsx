89a676a79d88b10212f2abe6b1696db982998f63
Yogender <yashmittal92002@gmail.com>
2025-08-23 11:25:26 +0530

    updated

--- src/components/Women.jsx
+++ src/components/Women.jsx
@@ -7,7 +7,7 @@ const Women = () => {
          <p>Women Items:</p>
          <ul>
      <li> <Link to="grooming">Grooming</Link></li>
-      <li><Link to="shirt">Shirt</Link></li>
+      <li><Link to="Shirt">Shirt</Link></li>
       <li><Link to="trouser">Trouser</Link></li>
       <li><Link to="jewellery">Jewellery</Link></li>
       </ul>
